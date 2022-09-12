95e9fd160a61fdf6f3dd8ad4e1ad12bea32dfb4b
irwin-deng <theirwindeng@gmail.com>
2022-09-12 04:15:49 +0000

    Add animations to landing section
    - Add typewriter effect to welcome message
    - Add bouncing arrow

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import Typewriter from 'typewriter-effect/dist/core';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  websiteWord: string = `
+  Hi, <br>
+  My name is <span 
+  style="background: -webkit-linear-gradient(45deg, var(--blue), var(--aqua));
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;"
+  >
+    Irwin Deng.
+  </span><br>
+  Welcome to my
+  <span 
+    style="background: -webkit-linear-gradient(45deg, var(--green), var(--lime));
+      background-clip: text;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;"
+  >
+    website!
+  </span>
+  `;
+
+  portfolioWord: string = `
+  <span 
+    style="background: -webkit-linear-gradient(45deg, var(--purple), var(--magenta));
+      background-clip: text;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;"
+  >
+    portfolio!
+  </span>
+  `;
+
+  constructor() {}
 
   ngOnInit(): void {
+    const elementWelcomeMessage = document.querySelector('#welcome-message');
+    var typewriter = new Typewriter(elementWelcomeMessage, {
+      loop: true,
+      delay: 25,
+    });
+    typewriter
+      .typeString(this.websiteWord)
+      .pauseFor(300)
+      .deleteChars(14)
+      .typeString(this.portfolioWord)
+      .pauseFor(300)
+      .start();
   }
-
 }
